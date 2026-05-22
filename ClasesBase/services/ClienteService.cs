8e6c9863294185742ff5559dcb077d8ab07c26c0
LisandroPR <lisandroperezruarte@gmail.com>
2026-05-22 12:56:13 -0300

    ABM-Agregando alta clientes

--- ClasesBase/services/ClienteService.cs
+++ ClasesBase/services/ClienteService.cs
@@ -1,40 +1,108 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace ClasesBase.services
 {
     public class ClienteService
     {
-        private static List<Cliente> clientes = new List<Cliente>();
+        private string cadenaConexion =
+            ConfigurationManager.ConnectionStrings["opticaConnection"].ConnectionString;
 
-        public static void AgregarCliente(Cliente p)
+        public DataTable CargarObrasSociales()
         {
+            string query = "SELECT CUIT, RAZON_SOCIAL FROM OBRAS_SOCIALES";
 
-            if (BuscarPordni(p.Cli_DNI) != null)
-            {
-                throw new Exception("El Cliente ya existe");
-            }
+            SqlDataAdapter da = new SqlDataAdapter(query, cadenaConexion);
+            DataTable dt = new DataTable();
 
-            clientes.Add(p);
+            da.Fill(dt);
+
+            return dt;
         }
 
-        public static List<Cliente> Obtenerclientes()
+        public void GuardarCliente(string dni, string apellido, string nombre,
+                                   string direccion, string nroCarnet, string osCuit)
         {
-            return clientes;
+            using (SqlConnection con = new SqlConnection(cadenaConexion))
+            {
+                string query = @"INSERT INTO CLIENTES
+                                (DNI, APELLIDO, NOMBRE, DIRECCION, NRO_CARNET, OS_CUIT)
+                                VALUES
+                                (@dni, @apellido, @nombre, @direccion, @carnet, @os)";
+
+                SqlCommand cmd = new SqlCommand(query, con);
+
+                cmd.Parameters.AddWithValue("@dni", dni);
+                cmd.Parameters.AddWithValue("@apellido", apellido);
+                cmd.Parameters.AddWithValue("@nombre", nombre);
+                cmd.Parameters.AddWithValue("@direccion", direccion);
+                cmd.Parameters.AddWithValue("@carnet", nroCarnet);
+                cmd.Parameters.AddWithValue("@os", osCuit);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
         }
 
-        public static Cliente BuscarPordni(string dni)
+        public DataTable BuscarClientes(string dni, string apellido)
         {
-            foreach (Cliente p in clientes)
+            string query = @"SELECT *
+                             FROM CLIENTES
+                             WHERE DNI LIKE @dni
+                             AND APELLIDO LIKE @apellido";
+
+            SqlDataAdapter da = new SqlDataAdapter(query, cadenaConexion);
+
+            da.SelectCommand.Parameters.AddWithValue("@dni", "%" + dni + "%");
+            da.SelectCommand.Parameters.AddWithValue("@apellido", "%" + apellido + "%");
+
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            return dt;
+        }
+
+        public void ModificarCliente(string dni, string apellido, string nombre,
+                                     string direccion, string nroCarnet, string osCuit)
+        {
+            using (SqlConnection con = new SqlConnection(cadenaConexion))
             {
-                if (p.Cli_DNI == dni)
-                {
-                    return p;
-                }
+                string query = @"UPDATE CLIENTES
+                                SET APELLIDO = @apellido,
+                                    NOMBRE = @nombre,
+                                    DIRECCION = @direccion,
+                                    NRO_CARNET = @carnet,
+                                    OS_CUIT = @os
+                                WHERE DNI = @dni";
+
+                SqlCommand cmd = new SqlCommand(query, con);
+
+                cmd.Parameters.AddWithValue("@dni", dni);
+                cmd.Parameters.AddWithValue("@apellido", apellido);
+                cmd.Parameters.AddWithValue("@nombre", nombre);
+                cmd.Parameters.AddWithValue("@direccion", direccion);
+                cmd.Parameters.AddWithValue("@carnet", nroCarnet);
+                cmd.Parameters.AddWithValue("@os", osCuit);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
             }
-            return null;
+        }
+
+        public void EliminarCliente(string dni)
+        {
+            using (SqlConnection con = new SqlConnection(cadenaConexion))
+            {
+                string query = "DELETE FROM CLIENTES WHERE DNI = @dni";
+
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@dni", dni);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
         }
     }
 }
