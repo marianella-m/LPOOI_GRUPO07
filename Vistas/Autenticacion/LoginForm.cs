ee79a4d24ba75d35dcde4c4a0967edcf0cbf4ee3
Marianella <dev.marianella@gmail.com>
2026-05-25 01:18:46 -0300

    doc: se sube documentacion de los metodos usados en el frontend del login

--- Vistas/Autenticacion/LoginForm.cs
+++ Vistas/Autenticacion/LoginForm.cs
@@ -25,38 +25,17 @@ namespace Vistas
             lblErrorContrasenia.Visible = false;
         }
 
-        List<Rol> roles = new List<Rol>();
-        List<Usuario> usuarios = new List<Usuario>();
+        // NAVEGACION ENTRE LOS CAMPOS
 
-        private void btnLogin_Click(object sender, EventArgs e) {
-            string username = txtNombreUsuario.Text.Trim();
-            string password = txtContrasenia.Text.Trim();
-
-            Usuario usuarioLoggeado = LoginService.loginDB(username, password);
-
-            if (usuarioLoggeado != null) {
-                new Home(usuarioLoggeado).Show();
-                this.Hide();
-            } else {
-                MessageBox.Show("El usuario o la contraseña son incorrectos. Verifique los datos.", "Error de Autenticación", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                txtNombreUsuario.Focus();
-            }
-        }
-
-        // Boton que muestra contraseña
-        private void btnShowHidePassword_Click(object sender, EventArgs e) {
-            if (txtContrasenia.PasswordChar.Equals('*')) {
-                txtContrasenia.PasswordChar = '\0';
-                btnShowHidePassword.Image = Properties.Resources.hide;
-            } else {
-                txtContrasenia.PasswordChar = '*';
-                btnShowHidePassword.Image = Properties.Resources.view__2_;
-            }
-        }
-
-        //Navegación
-        private void txtNombreUsuario_KeyDown(object sender, KeyEventArgs e) {
-            if (e.KeyCode == Keys.Enter) {
+        /// <summary>
+        /// Luego de validar que el campo nombre de usuario, no este vacio, avanza al siguiente campo al presionar enter.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void txtNombreUsuario_KeyDown(object sender, KeyEventArgs e) 
+        {
+            if (e.KeyCode == Keys.Enter) 
+            {
                 e.SuppressKeyPress = true;
 
                 if (validarCampoUsuario())
@@ -64,8 +43,15 @@ namespace Vistas
             }
         }
 
-        private void txtContrasenia_KeyDown(object sender, KeyEventArgs e) {
-            if (e.KeyCode == Keys.Enter) {
+        /// <summary>
+        /// Luego de validar que el campo contraseña, no este vacio, avanza al siguiente campo al presionar enter.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void txtContrasenia_KeyDown(object sender, KeyEventArgs e) 
+        {
+            if (e.KeyCode == Keys.Enter) 
+            {
                 e.SuppressKeyPress = true;
 
                 if (validarCampoContrasenia())
@@ -73,11 +59,18 @@ namespace Vistas
             }
         }
 
-        // Validacion de cambos no vacios
-        private bool validarCampoUsuario() {
+        // VALIDACIONES
+
+        /// <summary>
+        /// Retorna true si el campo nombre de usuario esta vacio.
+        /// </summary>
+        /// <returns></returns>
+        private bool validarCampoUsuario() 
+        {
             string nombreUsuario = txtNombreUsuario.Text.Trim();
 
-            if (string.IsNullOrEmpty(nombreUsuario)) {
+            if (string.IsNullOrEmpty(nombreUsuario)) 
+            {
                 lblErrorNombreUsuario.Visible = true;
                 txtNombreUsuario.Focus();
                 return false;
@@ -87,10 +80,16 @@ namespace Vistas
             return true;
         }
 
-        private bool validarCampoContrasenia() {
+        /// <summary>
+        /// Retorna true si el campo contraseña esta vacio.
+        /// </summary>
+        /// <returns></returns>
+        private bool validarCampoContrasenia() 
+        {
             string contrasenia = txtContrasenia.Text.Trim();
 
-            if (string.IsNullOrEmpty(contrasenia)) {
+            if (string.IsNullOrEmpty(contrasenia)) 
+            {
                 lblErrorContrasenia.Visible = true;
                 txtContrasenia.Focus();
                 return false;
@@ -98,13 +97,64 @@ namespace Vistas
             
             lblErrorContrasenia.Visible = false;
             return true;
-       }
+        }
 
-        private void LoginForm_FormClosing(object sender, FormClosingEventArgs e) {
+        // BOTONES
+
+        /// <summary>
+        /// Muestra el contenido del campo contraseña.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnShowHidePassword_Click(object sender, EventArgs e) 
+        {
+            if (txtContrasenia.PasswordChar.Equals('*')) 
+            {
+                txtContrasenia.PasswordChar = '\0';
+                btnShowHidePassword.Image = Properties.Resources.hide;
+            } 
+            else 
+            {
+                txtContrasenia.PasswordChar = '*';
+                btnShowHidePassword.Image = Properties.Resources.view__2_;
+            }
+        }
+
+        /// <summary>
+        /// Mensaje de confirmacion luego de cerrar la ventana Inicio Sesion.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void LoginForm_FormClosing(object sender, FormClosingEventArgs e) 
+        {
             DialogResult dialogResult = MessageBox.Show("¿Está seguro que desea salir?", "Alerta", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
 
             if (dialogResult == DialogResult.Cancel)
                 e.Cancel = true;
+        }
+
+        /// <summary>
+        /// Luego de validar las credenciales de un usuario redirecciona al Menu Principal
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnLogin_Click(object sender, EventArgs e) 
+        {
+            string username = txtNombreUsuario.Text.Trim();
+            string password = txtContrasenia.Text.Trim();
+
+            Usuario usuarioLoggeado = LoginService.loginDB(username, password);
+
+            if (usuarioLoggeado != null) 
+            {
+                new Home(usuarioLoggeado).Show();
+                this.Hide();
+            } 
+            else 
+            {
+                MessageBox.Show("El usuario o la contraseña son incorrectos. Verifique los datos.", "Error de Autenticación", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtNombreUsuario.Focus();
+            }
         }
     }
 }
