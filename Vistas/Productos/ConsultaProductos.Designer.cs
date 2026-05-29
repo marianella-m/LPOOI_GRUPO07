7567247e5e5fb7ade2442914571b705640412754
LisandroPR <lisandroperezruarte@gmail.com>
2026-05-29 10:55:03 -0300

    Agregando SP para ordenar productos por descripcion o categoria y botones en ConsultaProductos.cs

--- Vistas/Productos/ConsultaProductos.Designer.cs
+++ Vistas/Productos/ConsultaProductos.Designer.cs
@@ -34,6 +34,10 @@
             this.btnBuscar = new System.Windows.Forms.Button();
             this.btnBorrar = new System.Windows.Forms.Button();
             this.btnModificar = new System.Windows.Forms.Button();
+            this.rbDescripcion = new System.Windows.Forms.RadioButton();
+            this.rbCategoria = new System.Windows.Forms.RadioButton();
+            this.btnConsultar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dgvProductos)).BeginInit();
             this.SuspendLayout();
             // 
@@ -43,12 +47,12 @@
             this.dgvProductos.AllowUserToDeleteRows = false;
             this.dgvProductos.AllowUserToResizeColumns = false;
             this.dgvProductos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvProductos.Location = new System.Drawing.Point(16, 74);
-            this.dgvProductos.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dgvProductos.Location = new System.Drawing.Point(15, 197);
+            this.dgvProductos.Margin = new System.Windows.Forms.Padding(4);
             this.dgvProductos.Name = "dgvProductos";
             this.dgvProductos.ReadOnly = true;
             this.dgvProductos.Size = new System.Drawing.Size(583, 239);
-            this.dgvProductos.TabIndex = 0;
+            this.dgvProductos.TabIndex = 5;
             // 
             // label6
             // 
@@ -57,62 +61,108 @@
             this.label6.Location = new System.Drawing.Point(9, 16);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(290, 29);
-            this.label6.TabIndex = 19;
+            this.label6.TabIndex = 9;
             this.label6.Text = "Consulta de Productos";
             // 
             // txtBuscar
             // 
-            this.txtBuscar.Location = new System.Drawing.Point(355, 17);
-            this.txtBuscar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtBuscar.Location = new System.Drawing.Point(352, 19);
+            this.txtBuscar.Margin = new System.Windows.Forms.Padding(4);
             this.txtBuscar.Name = "txtBuscar";
             this.txtBuscar.Size = new System.Drawing.Size(112, 22);
-            this.txtBuscar.TabIndex = 20;
+            this.txtBuscar.TabIndex = 0;
             // 
             // btnBuscar
             // 
             this.btnBuscar.Location = new System.Drawing.Point(499, 11);
-            this.btnBuscar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnBuscar.Margin = new System.Windows.Forms.Padding(4);
             this.btnBuscar.Name = "btnBuscar";
             this.btnBuscar.Size = new System.Drawing.Size(100, 36);
-            this.btnBuscar.TabIndex = 21;
+            this.btnBuscar.TabIndex = 1;
             this.btnBuscar.Text = "Buscar";
             this.btnBuscar.UseVisualStyleBackColor = true;
             this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click_1);
             // 
             // btnBorrar
             // 
-            this.btnBorrar.Location = new System.Drawing.Point(355, 320);
-            this.btnBorrar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnBorrar.Location = new System.Drawing.Point(355, 464);
+            this.btnBorrar.Margin = new System.Windows.Forms.Padding(4);
             this.btnBorrar.Name = "btnBorrar";
             this.btnBorrar.Size = new System.Drawing.Size(100, 32);
-            this.btnBorrar.TabIndex = 22;
+            this.btnBorrar.TabIndex = 6;
             this.btnBorrar.Text = "Borrar";
             this.btnBorrar.UseVisualStyleBackColor = true;
             this.btnBorrar.Click += new System.EventHandler(this.btnBorrar_Click_1);
             // 
             // btnModificar
             // 
-            this.btnModificar.Location = new System.Drawing.Point(479, 320);
-            this.btnModificar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnModificar.Location = new System.Drawing.Point(499, 464);
+            this.btnModificar.Margin = new System.Windows.Forms.Padding(4);
             this.btnModificar.Name = "btnModificar";
             this.btnModificar.Size = new System.Drawing.Size(100, 32);
-            this.btnModificar.TabIndex = 23;
+            this.btnModificar.TabIndex = 7;
             this.btnModificar.Text = "Modificar";
             this.btnModificar.UseVisualStyleBackColor = true;
             this.btnModificar.Click += new System.EventHandler(this.btnModificar_Click_1);
+            // 
+            // rbDescripcion
+            // 
+            this.rbDescripcion.AutoSize = true;
+            this.rbDescripcion.Location = new System.Drawing.Point(16, 87);
+            this.rbDescripcion.Name = "rbDescripcion";
+            this.rbDescripcion.Size = new System.Drawing.Size(103, 21);
+            this.rbDescripcion.TabIndex = 2;
+            this.rbDescripcion.TabStop = true;
+            this.rbDescripcion.Text = "Descripción";
+            this.rbDescripcion.UseVisualStyleBackColor = true;
+            // 
+            // rbCategoria
+            // 
+            this.rbCategoria.AutoSize = true;
+            this.rbCategoria.Location = new System.Drawing.Point(16, 116);
+            this.rbCategoria.Name = "rbCategoria";
+            this.rbCategoria.Size = new System.Drawing.Size(90, 21);
+            this.rbCategoria.TabIndex = 3;
+            this.rbCategoria.TabStop = true;
+            this.rbCategoria.Text = "Categoría";
+            this.rbCategoria.UseVisualStyleBackColor = true;
+            // 
+            // btnConsultar
+            // 
+            this.btnConsultar.Location = new System.Drawing.Point(16, 145);
+            this.btnConsultar.Name = "btnConsultar";
+            this.btnConsultar.Size = new System.Drawing.Size(100, 32);
+            this.btnConsultar.TabIndex = 4;
+            this.btnConsultar.Text = "Consultar";
+            this.btnConsultar.UseVisualStyleBackColor = true;
+            this.btnConsultar.Click += new System.EventHandler(this.btnOrdenar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(11, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(188, 17);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Consultar productos por:";
             // 
             // ConsultaProductos
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(615, 367);
+            this.ClientSize = new System.Drawing.Size(618, 525);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnConsultar);
+            this.Controls.Add(this.rbCategoria);
+            this.Controls.Add(this.rbDescripcion);
             this.Controls.Add(this.btnModificar);
             this.Controls.Add(this.btnBorrar);
             this.Controls.Add(this.btnBuscar);
             this.Controls.Add(this.txtBuscar);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.dgvProductos);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "ConsultaProductos";
             this.Text = "ConsultaProductos";
             this.Load += new System.EventHandler(this.ConsultaProductos_Load);
@@ -130,5 +180,9 @@
         private System.Windows.Forms.Button btnBuscar;
         private System.Windows.Forms.Button btnBorrar;
         private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.RadioButton rbDescripcion;
+        private System.Windows.Forms.RadioButton rbCategoria;
+        private System.Windows.Forms.Button btnConsultar;
+        private System.Windows.Forms.Label label1;
     }
 }
